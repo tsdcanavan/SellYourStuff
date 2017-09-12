ccd1665b439c60270864cec3347b94991d75bd6f
unknown <tsdcanavan@yahoo.com>
2017-09-12 13:55:33 -0400

    added firebase logic

--- assets/js/sellstuff.js
+++ assets/js/sellstuff.js
@@ -2,23 +2,71 @@
 var zipInput = "08873";
 var zipArray = [];
 var zipResponse = false;
-
+var userId;
 function initFirebase() {
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyAcRiiCDBcNgsBJA-ZQYa3jCNWh9u1YbNY",
-    authDomain: "sellyourstuff-d04db.firebaseapp.com",
-    databaseURL: "https://sellyourstuff-d04db.firebaseio.com",
-    projectId: "sellyourstuff-d04db",
-    storageBucket: "sellyourstuff-d04db.appspot.com",
-    messagingSenderId: "37415365376"
-};
-firebase.initializeApp(config);
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAcRiiCDBcNgsBJA-ZQYa3jCNWh9u1YbNY",
+        authDomain: "sellyourstuff-d04db.firebaseapp.com",
+        databaseURL: "https://sellyourstuff-d04db.firebaseio.com",
+        projectId: "sellyourstuff-d04db",
+        storageBucket: "sellyourstuff-d04db.appspot.com",
+        messagingSenderId: "37415365376"
+    };
+    firebase.initializeApp(config);
 
-   // Get a reference to the database service
-   database = firebase.database();
+    // Get a reference to the database service
+    database = firebase.database();
 
 }
+
+function addUser() {
+    // add user and get the users key
+    database.ref().push({
+        userName: "my name",
+        userEmail: "email@email.com",
+        userPwd: "xxxyyy",
+        userLocation: "city, state,zip"
+    });
+
+    database.ref().on('child_added', function (snapshot) {
+        userId = snapshot.key;
+        console.log(userId);
+    });
+}
+
+function addUser(loginName) {
+    // login user and get the users key
+    database.ref().on('value', function (snapshot) {
+        if (userName === loginName) {
+            userId = snapshot.key;
+            loginSuccess = true;
+            return
+        }
+    });
+    if (userId === "") {
+        loginSuccess = false;
+    }
+}
+
+function addItem(userId) {
+    // add an item to the user's account and get the item's key
+    database.ref(userId).push({
+        itemName: "chair",
+        itemDesc: "sitdown",
+        itemQty: 2,
+        itemPrice: 85.00,
+        itemTags: "tagger1, tagger2, tagger3",
+        itemImg: "url infor"
+    });
+    database.ref(userId).on('child_added', function (snapshot) {
+        itemId = snapshot.key;
+        console.log(itemId);
+    });
+
+}
+
+
 
 if (zipInput === "") {
     zipInput = "08873";
@@ -45,6 +93,9 @@ if (zipInput.length === 5) {
             for (i = 0; i < response.zip_codes.length; i++) {
                 zipArray[i] = response.zip_codes[i].zip_code;
             }
+            database.ref().on("value", function (snapshot) {
+                console.log("inside firebase read");
+            });    
         }
     });
 }
