5985262c27e660db1cba07f6fae0b9cf7935c88b
unknown <tsdcanavan@yahoo.com>
2017-09-16 11:42:49 -0400

    sellstuff.js updating

--- assets/js/sellstuff.js
+++ assets/js/sellstuff.js
@@ -52,47 +52,11 @@ function loginUser(loginEmail) {
     return loginSucess, "0";
 }
 
-// function addUser(loginEmail, zipCode, password) {
-//     // login user and get the users key
-//     database.ref().on('child_added', function (snapshot) {
-//         if (userEmail === loginEmail) {
-//             userId = snapshot.key;
-//             loginSuccess = false;
-//             return loginSuccess;
-//         }
-//     });
-//     var request = "https://www.zipcodeapi.com/rest/" +
-//         "js-wAyUdNRpP6Np73vS03R6gNZ4yl9v22jyDStRFlgvr4Uz7qs8tkeK7eOGzYcC1vbE/" +
-//         "radius.json/" + zipCode + "/1/mile";
-//     $.ajax({
-//         url: request,
-//         method: "GET"
-//     }).done(function (response) {
-//         for (i = 0; i < zip_codes.length; i++) {
-//             if (response.zip_codes[i].distance === 0) {
-//                 var apiCity = response.zip_codes[i].city;
-//                 var apiState = respons.zip_codes[i].state;
-//             }
-//         }
-//         console.log(response);
-//     }
-//     database.ref().push({
-//             userName: "",
-//             userEmail: loginEmail,
-//             userPwd: password,
-//             userLocation: {
-//                 city: apiCity,
-//                 state: apistate,
-//                 zip: zipCode
-//             }
-//         });
-//     loginSuccess = false;
-// }
 
-function addUser(loginEmail, zipCode, password, name) {
+function addUser(loginEmail, name, zipCode, userId) {
     // login user and get the users key
     var loginEmail;
-    var password;
+    var userId;
     var zipCode;
     var name;
 
@@ -109,23 +73,15 @@ function addUser(loginEmail, zipCode, password, name) {
                 var apiCity = response.zip_codes[i].city;
                 var apiState = response.zip_codes[i].state;
                 var apiZip = response.zip_codes[i].zip_code;
-                database.ref().push({
+                database.ref(userId).set({
                     userName: name,
                     userEmail: loginEmail,
-                    userPwd: password,
                     userLocation: {
                         city: apiCity,
                         state: apiState,
                         zip: apiZip
                     }
                 });
-                database.ref().on('child_added', function (snapshot) {
-                    if (userEmail === loginEmail) {
-                        userId = snapshot.key;
-                        return  userId;
-                    }
-                });
-                            
             }
         }
     });
@@ -134,18 +90,13 @@ function addUser(loginEmail, zipCode, password, name) {
 function addItem(userId, itemName, itemDesc, itemQty, itemPrice, itemTags, itemImg) {
     // add an item to the user's account and get the item's key
     database.ref(userId).push({
-        itemName: "chair",
-        itemDesc: "sitdown",
-        itemQty: 2,
-        itemPrice: 85.00,
-        itemTags: "tagger1, tagger2, tagger3",
-        itemImg: "url infor"
+        itemName: itemName,
+        itemDesc: itemDesc,
+        itemQty: itemQty,
+        itemPrice: itemPrice,
+        itemTags: itemTags,
+        itemImg: itemImg
     });
-    database.ref(userId).on('child_added', function (snapshot) {
-        itemId = snapshot.key;
-        console.log(itemId);
-    });
-
 }
 
 function register(){
@@ -160,6 +111,46 @@ function register(){
  console.log("userLocation ", userLocation);
     
     }
+
+
+    function zipLookupTest() {
+        // testing zip lookup - currently not called anywhere
+        if (zipInput === "") {
+            zipInput = "08873";
+        }
+        
+        console.log(zipInput.length);
+        if (zipInput.length === 5) {
+            // zipcodeapi.com   -   zip code api
+            var request = "https://www.zipcodeapi.com/rest/" +
+                "js-wAyUdNRpP6Np73vS03R6gNZ4yl9v22jyDStRFlgvr4Uz7qs8tkeK7eOGzYcC1vbE/" +
+                "radius.json/" + zipInput + "/10/mile";
+            console.log(request);
+            console.log(zipInput);
+            $.ajax({
+                url: request,
+                method: "GET"
+            }).done(function (response) {
+                if (response.zip_codes.length === 0) {
+                    alert("failed zip lookup");
+                    zipResponse = false;
+                } else {
+                    zipResponse = true;
+                    console.log(response);
+                    for (i = 0; i < response.zip_codes.length; i++) {
+                        zipArray[i] = response.zip_codes[i].zip_code;
+                    }
+                    database.ref().on("value", function (snapshot) {
+                        console.log("inside firebase read");
+                    });
+                }
+            });
+        }
+        if (zipResponse === false || zipInput.length != 5) {
+            //search itemName, itemDesc, itemTags
+        }
+        }
+        
 initFirebase();
 
 
@@ -167,48 +158,19 @@ initFirebase();
 $('#regSend').on('click', function(){
     register();
     //I am placing my user login here to replace yours
-firebase.auth().createUserWithEmailAndPassword(userEmail, userPwd).catch(function(error) {
+    var registered=true;
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPwd).catch(function(error) {
     // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
+    registered=false;
     // ...
   });
+  if (registered === true) {
+    userId = firebase.auth().currentUser;
+    addUser(userEmail, userName, userLocation, userId )
+  }
 });
 
 
 
-
-if (zipInput === "") {
-    zipInput = "08873";
-}
-
-console.log(zipInput.length);
-if (zipInput.length === 5) {
-    // zipcodeapi.com   -   zip code api
-    var request = "https://www.zipcodeapi.com/rest/" +
-        "js-wAyUdNRpP6Np73vS03R6gNZ4yl9v22jyDStRFlgvr4Uz7qs8tkeK7eOGzYcC1vbE/" +
-        "radius.json/" + zipInput + "/10/mile";
-    console.log(request);
-    console.log(zipInput);
-    $.ajax({
-        url: request,
-        method: "GET"
-    }).done(function (response) {
-        if (response.zip_codes.length === 0) {
-            alert("failed zip lookup");
-            zipResponse = false;
-        } else {
-            zipResponse = true;
-            console.log(response);
-            for (i = 0; i < response.zip_codes.length; i++) {
-                zipArray[i] = response.zip_codes[i].zip_code;
-            }
-            database.ref().on("value", function (snapshot) {
-                console.log("inside firebase read");
-            });
-        }
-    });
-}
-if (zipResponse === false || zipInput.length != 5) {
-    //search itemName, itemDesc, itemTags
-}
